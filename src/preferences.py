0ca58e0b1f3937f0ffc4e7b5ac49e1e4abcdc46a
Alberto Malagoli <alberto.malagoli@daqri.com>
2015-11-23 17:01:48 +0000

    preferences code refactoring

--- src/preferences.py
+++ src/preferences.py
@@ -2,120 +2,95 @@ from PyQt5.QtCore import QSettings
 
 __author__ = "Alberto Malagoli"
 
-_WORDS_SEPARATOR = "words_separator"
-_LANGUAGE_REPRESENTATION = "language_representation"
-_DURATION_REPRESENTATION = "duration_representation"
-_STATS_AGREEMENT = "stats_agreement"
-_RENAMING_RULE = "renaming_rule"
-_FIRST_TIME_OPENING = "first_time_opening"
-_LAST_VISITED_DIRECTORY = "last_visited_directory"
-
 
 class Preferences:
     """
 
     """
 
+    DURATION_REPRESENTATION_MINUTES = 0
+    DURATION_REPRESENTATION_HOURS_MINUTES = 1
+
+    __WORDS_SEPARATOR = "words_separator"
+    __LANGUAGE_REPRESENTATION = "language_representation"
+    __DURATION_REPRESENTATION = "duration_representation"
+    __STATS_AGREEMENT = "stats_agreement"
+    __RENAMING_RULE = "renaming_rule"
+    __FIRST_TIME_OPENING = "first_time_opening"
+    __LAST_VISITED_DIRECTORY = "last_visited_directory"
+
     def __init__(self):
-        self._preferences = QSettings("preferences.ini", QSettings.IniFormat)
+        self.__preferences = QSettings("preferences.ini", QSettings.IniFormat)
 
     #
     # last_visited_directory
     #
 
-    def get_last_visited_directory(self):
-        return self._preferences.value(_LAST_VISITED_DIRECTORY, "")
+    def get_last_visited_directory(self) -> str:
+        return self.__preferences.value(Preferences.__LAST_VISITED_DIRECTORY, "")
 
-    def set_last_visited_directory(self, value):
-        """
-        Args:
-            value: String
-        """
-        self._preferences.setValue(_LAST_VISITED_DIRECTORY, value)
+    def set_last_visited_directory(self, value: str):
+        self.__preferences.setValue(Preferences.__LAST_VISITED_DIRECTORY, value)
 
     #
     # first_time_opening
     #
 
-    def get_first_time_opening(self):
-        return self._preferences.value(_FIRST_TIME_OPENING, True).toBool()
+    def get_first_time_opening(self) -> bool:
+        return self.__preferences.value(Preferences.__FIRST_TIME_OPENING, True).toBool()
 
-    def set_first_time_opening(self, value):
-        """
-        Args:
-            value: Bool
-        """
-        self._preferences.setValue(_FIRST_TIME_OPENING, value)
+    def set_first_time_opening(self, value: bool):
+        self.__preferences.setValue(Preferences.__FIRST_TIME_OPENING, value)
 
     #
     # renaming_rule
     #
 
-    def get_renaming_rule(self):
-        return self._preferences.value(_RENAMING_RULE, "")
+    def get_renaming_rule(self) -> str:
+        return self.__preferences.value(Preferences.__RENAMING_RULE, "")
 
-    def set_renaming_rule(self, value):
-        """
-        Args:
-            value: String
-        """
-        self._preferences.setValue(_RENAMING_RULE, value)
+    def set_renaming_rule(self, value: str):
+        self.__preferences.setValue(Preferences.__RENAMING_RULE, value)
 
     #
     # stats_agreement
     #
 
-    def get_stats_agreement(self):
-        return self._preferences.value(_STATS_AGREEMENT, False).toBool()
+    def get_stats_agreement(self) -> bool:
+        return self.__preferences.value(Preferences.__STATS_AGREEMENT, False).toBool()
 
-    def set_stats_agreement(self, value):
-        """
-        Args:
-            value: Bool
-        """
-        self._preferences.setValue(_STATS_AGREEMENT, value)
+    def set_stats_agreement(self, value: bool):
+        self.__preferences.setValue(Preferences.__STATS_AGREEMENT, value)
 
     #
     # duration_representation
     #
 
-    def get_duration_representation(self):
-        return self._preferences.value(_DURATION_REPRESENTATION, 0).toInt()
+    def get_duration_representation(self) -> int:
+        return self.__preferences.value(Preferences.__DURATION_REPRESENTATION, 0)
 
-    def set_duration_representation(self, value):
-        """
-        Args:
-            value: Int
-        """
-        self._preferences.setValue(_DURATION_REPRESENTATION, value)
+    def set_duration_representation(self, value: int):
+        self.__preferences.setValue(Preferences.__DURATION_REPRESENTATION, value)
 
     #
     # language_representation
     #
 
-    def get_language_representation(self):
-        return self._preferences.value(_LANGUAGE_REPRESENTATION, 0).toInt()
+    def get_language_representation(self) -> int:
+        return self.__preferences.value(Preferences.__LANGUAGE_REPRESENTATION, 0).toInt()
 
-    def set_language_representation(self, value):
-        """
-        Args:
-            value: Int
-        """
-        self._preferences.setValue(_LANGUAGE_REPRESENTATION, value)
+    def set_language_representation(self, value: int):
+        self.__preferences.setValue(Preferences.__LANGUAGE_REPRESENTATION, value)
 
     #
     # words_separator
     #
 
-    def get_words_separator(self):
-        return self._preferences.value(_WORDS_SEPARATOR, 0).toInt()
+    def get_words_separator(self) -> int:
+        return self.__preferences.value(Preferences.__WORDS_SEPARATOR, 0).toInt()
 
-    def set_words_separator(self, value):
-        """
-        Args:
-            value: Int
-        """
-        self._preferences.setValue(_WORDS_SEPARATOR, value)
+    def set_words_separator(self, value: int):
+        self.__preferences.setValue(Preferences.__WORDS_SEPARATOR, value)
 
 
 preferences = Preferences()
