c34d33dc645ccd04629f46a4a4214cdecee794b8
Alberto Malagoli <alberto.malagoli@daqri.com>
2015-11-27 17:11:33 +0000

    code refactoring

--- src/ui/main_window_view.py
+++ src/ui/main_window_view.py
@@ -7,13 +7,6 @@ from movie_table_item import MovieTableItem
 
 
 class MainWindowView:
-    __MOVIES_TABLE_VIEW_MODEL_CONTEXT_PROPERTY = "moviesTableViewModel"
-    __ERROR_PANEL_VISIBLE_CONTEXT_PROPERTY = "movieErrorPanelVisible"
-    __RENAMED_PANEL_VISIBLE_CONTEXT_PROPERTY = "movieRenamedPanelVisible"
-    __MOVIE_INFO_PANEL_VISIBLE_CONTEXT_PROPERTY = "movieInfoPanelVisible"
-    __LOADING_PANEL_VISIBLE_CONTEXT_PROPERTY = "loadingPanelVisible"
-    __LOADING_FILE_NAME_CONTEXT_PROPERTY = "loadingFileName"
-
     def __init__(self):
         self.__loading_file_name = ""
         self.__loading_panel_visible = True
@@ -24,18 +17,12 @@ class MainWindowView:
 
         self.__engine = QQmlApplicationEngine()
 
-        self.__set_context_property(MainWindowView.__LOADING_FILE_NAME_CONTEXT_PROPERTY,
-                                    self.__loading_file_name)
-        self.__set_context_property(MainWindowView.__LOADING_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__loading_panel_visible)
-        self.__set_context_property(MainWindowView.__MOVIE_INFO_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_info_panel_visible)
-        self.__set_context_property(MainWindowView.__RENAMED_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_renamed_panel_visible)
-        self.__set_context_property(MainWindowView.__ERROR_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_error_panel_visible)
-        self.__set_context_property(MainWindowView.__MOVIES_TABLE_VIEW_MODEL_CONTEXT_PROPERTY,
-                                    self.__movies_table_view_model)
+        self.__set_context_property("loadingFileName", self.__loading_file_name)
+        self.__set_context_property("loadingPanelVisible", self.__loading_panel_visible)
+        self.__set_context_property("movieInfoPanelVisible", self.__movie_info_panel_visible)
+        self.__set_context_property("movieRenamedPanelVisible", self.__movie_renamed_panel_visible)
+        self.__set_context_property("movieErrorPanelVisible", self.__movie_error_panel_visible)
+        self.__set_context_property("moviesTableViewModel", self.__movies_table_view_model)
 
         self.__engine.load("main_window.qml")
 
@@ -44,37 +31,31 @@ class MainWindowView:
 
     def set_loading_file_name(self, loading_file_name: str) -> None:
         self.__loading_file_name = loading_file_name
-        self.__set_context_property(MainWindowView.__LOADING_FILE_NAME_CONTEXT_PROPERTY,
-                                    self.__loading_file_name)
+        self.__set_context_property("loadingFileName", self.__loading_file_name)
 
     def set_loading_panel_visible(self, loading_panel_visible: bool) -> None:
         self.__loading_panel_visible = loading_panel_visible
-        self.__set_context_property(MainWindowView.__LOADING_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__loading_panel_visible)
+        self.__set_context_property("loadingPanelVisible", self.__loading_panel_visible)
 
     def set_movie_info_panel_visible(self, movie_info_panel_visible: bool) -> None:
         self.__movie_info_panel_visible = movie_info_panel_visible
-        self.__set_context_property(MainWindowView.__MOVIE_INFO_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_info_panel_visible)
+        self.__set_context_property("movieInfoPanelVisible", self.__movie_info_panel_visible)
 
     def set_movie_renamed_panel_visible(self, movie_renamed_panel_visible: bool) -> None:
         self.__movie_renamed_panel_visible = movie_renamed_panel_visible
-        self.__set_context_property(MainWindowView.__RENAMED_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_renamed_panel_visible)
+        self.__set_context_property("movieRenamedPanelVisible", self.__movie_renamed_panel_visible)
 
     def set_movie_error_panel_visible(self, movie_error_panel_visible: bool) -> None:
         self.__movie_error_panel_visible = movie_error_panel_visible
-        self.__set_context_property(MainWindowView.__ERROR_PANEL_VISIBLE_CONTEXT_PROPERTY,
-                                    self.__movie_error_panel_visible)
+        self.__set_context_property("movieErrorPanelVisible", self.__movie_error_panel_visible)
 
-    def add_movie_table_item(self, original_name: str, new_name: str) -> None:
+    def add_movie_table_item(self, original_name: str, new_name: str):
         movie_table_item = MovieTableItem(original_name, new_name)
         self.__movies_table_view_model.append(movie_table_item)
         # From Qt Documentation:
         # Note: There is no way for the view to know that the contents of a QList has changed.
         # If the QList changes, it is necessary to reset the model by calling QQmlContext::setContextProperty() again.
-        self.__set_context_property(MainWindowView.__MOVIES_TABLE_VIEW_MODEL_CONTEXT_PROPERTY,
-                                    self.__movies_table_view_model)
+        self.__set_context_property("moviesTableViewModel", self.__movies_table_view_model)
 
 
 app = QGuiApplication(sys.argv)
